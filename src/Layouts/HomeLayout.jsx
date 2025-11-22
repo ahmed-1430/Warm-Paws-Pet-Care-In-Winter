c2ae46607a3310714a3281de2b77640a0289fb12
ahmed-1430 <ahmedtexting@gmail.com>
2025-11-22 11:46:28 +0600

    Re render Issues fiex

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
-import { Outlet, useNavigate } from 'react-router';
 import Footer from '../Components/Footer';
 import LoadingPage from '../Pages/Loading';
+import { Outlet, useNavigation } from 'react-router';
 
 const HomeLayout = () => {
-     const { state } = useNavigate()
-    return (
-        <div className='flex flex-col h-fit bg-linear-to-br from-slate-50 to-blue-50'>
-            <header>
-                <nav className='z-50 relative'>
-                    <Navbar></Navbar>
-                </nav>
-            </header>
-             <main className='flex-1'>
-                {
-                    state == "loading" ? <LoadingPage></LoadingPage> : <Outlet></Outlet>
-                }
-                    <Outlet></Outlet>
-                </main>
-                <footer>
-                    <Footer></Footer>
-                </footer>
-        </div>
-    );
+  const navigation = useNavigation();
+
+  return (
+    <div className='flex flex-col h-fit bg-linear-to-br from-slate-50 to-blue-50'>
+      <header>
+        <nav className='z-50 relative'>
+          <Navbar />
+        </nav>
+      </header>
+
+      <main className='flex-1'>
+        {navigation.state === "loading"
+          ? <LoadingPage />
+          : <Outlet />
+        }
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default HomeLayout;
